d95e58bc6beee06222286603b2074b6934302d2d
cade-101 <cj_sander@outlook.com>
2026-03-30 21:16:43 -0600

    mar 30

--- App.tsx
+++ App.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import WorkdayRhythm from './src/screens/WorkdayRhythm';
+import QuickHits from './src/screens/QuickHits';
+
+<Tab.Screen name="Hits" component={QuickHits} options={{ tabBarIcon: () => <Text>💥</Text> }} />
 
 export default function App() {
   return <WorkdayRhythm />;
